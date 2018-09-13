8552382e7c8e56100ddc362ad93986071d4bc9cc
Chi Hoang <chhoang102@gmail.com>
2018-09-12 22:43:33 -0700

    Merge and update README file

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# imageclassifier
-Image classifier project as part of AI Programming with Python on Udacity
+# Image Classifier  
+# Developing an AI application
+
+Going forward, AI algorithms will be incorporated into more and more everyday applications. For example, you might want to include an image classifier in a smart phone app. To do this, you'd use a deep learning model trained on hundreds of thousands of images as part of the overall application architecture. A large part of software development in the future will be using these types of models as common parts of applications. 
+
+In this project, we'll train an image classifier to recognize different species of flowers. You can imagine using something like this in a phone app that tells you the name of the flower your camera is looking at. In practice you'd train this classifier, then export it for use in your application. We'll be using [this dataset](http://www.robots.ox.ac.uk/~vgg/data/flowers/102/index.html) of 102 flower categories.
+
+<img src='assets/Flowers.png' width=500px>
+
+![Alt text](assets/Flowers.png?raw=true "Title")
+
+The project is broken down into multiple steps:
+
+* Load and preprocess the image dataset
+* Train the image classifier on your dataset
+* Use the trained classifier to predict image content
+
+This is an application that can be trained on any set of labeled images. Here your network will be learning about flowers and end up as a command line application. But, what you do with your new skills depends on your imagination and effort in building a dataset. For example, imagine an app where you take a picture of a car, it tells you what the make and model is, then looks up information about it.
+
+I have trained my models and saved them as checkpoints that can be used to predict a flower species from an image. Basic usage can be found below.
+
+# Dependencies (via conda recommended)
+Standard Python 3 package
+matplotlib
+torch
+torchvision
+
+i.e. conda install python numpy pandas matplotlib torch torchvision
+
+# Below is the result of my model training on the flower dataset with (kind of) various architectures and hyperparameters
+| Architecture | Epochs | Hidden Layers | Learning Rate | Accuracy | Saved                                           |
+| ------------ | ------ | ------------- | ------------- | -------- | ----------------------------------------------- |
+| VGG16        | 3      | [2048, 1024]  | 0.001         | 76.07%   |                                                 |
+| VGG16        | 1      | [4096, 2048]  | 0.001         | 64.10%   |                                                 |
+| VGG16        | 1      | [1, 1024]     | 0.001         | 0.49%    |                                                 |
+| VGG16        | 1      | [1024, 2048]  | 0.001         | 1.34%    |                                                 |
+| VGG16        | 2      | [1024, 2048]  | 0.001         | 1.46%    |                                                 |
+| VGG16        | 4      | [1024]        | 0.001         | 82.78%   |                                                 |
+| VGG16        | 5      | [1024]        | 0.001         | 85.47%   |                                                 |
+| DenseNet169  | 5      | [1024]        | 0.001         | 93.52%   |                                                 |
+| DenseNet169  | 5      | [1024]        | 0.001         | 92.06%   | densenet169_pretrained_checkpoint.pth           |
+| DenseNet169  | 4      | [1024]        | 0.001         | 90.60%   | DenseNet_1664_[1024]_102_5_0.001_checkpoint.pth |
+
+
+# Basic Usages
+```
+$ python predict.py [path to your image] [path to checkpoint] [optional flags]
+```
+
+
+--top_k - number of classes with top highest probabilities  
+--gpu - whether to utilize gpu to train
+
+i.e.
+```  
+$ python predict.py img001.jpg DenseNet_1664_[1024]_102_5_0.001_checkpoint.pth --top_k 3
+```
+
+
+
+
+
+
--- predict.py
+++ predict.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#                                                                             
+#
 # PROGRAMMER: Chi Hoang
 # DATE CREATED: 09/09/2018
 # REVISED DATE: 09/10/2018 <=(Date Revised - if any)
-    # Remove --arch flag and load model from the checkpoint info 
+# Remove --arch flag and load model from the checkpoint info
 # PURPOSE: Use a trained network to predict the class for an input image
 '''
 Basic usage: python predict.py /path/to/image checkpoint
@@ -37,33 +37,47 @@ from torch import optim
 import utilities
 import fc_model
 
+
 def main():
     in_arg = get_input_args()
-    
+
     image_path = in_arg.input
     checkpoint_path = in_arg.checkpoint
     category_names = in_arg.category_names
     topk = in_arg.top_k
-    device = 'gpu' if in_arg.gpu else 'cpu'
-    
+
+    if(in_arg.gpu):
+        if torch.cuda.is_available():
+            device = 'gpu'
+            print('Using GPU for calculations')
+        else:
+            device = 'cpu'
+            print('Your system is not compatible with CUDA')
+            print('Using CPU for calculations')
+    else:
+        device = 'cpu'
+        print('Using CPU for calculations')
+
     # Load checkpoint and rebuild our model
     saved_model, checkpoint = fc_model.load_checkpoint(checkpoint_path)
     print('Model loaded successfully!')
-    
+
     # Get index to class map
     class_to_idx = checkpoint['class_to_idx']
-    idx_to_class = {v : k for k, v in class_to_idx.items()}
-    
+    idx_to_class = {v: k for k, v in class_to_idx.items()}
+
     # Predict input image
-    probs, classes = predict(image_path, saved_model, idx_to_class, topk, device)
-    
+    probs, classes = predict(image_path, saved_model,
+                             idx_to_class, topk, device)
+
     cat_to_name = fc_model.map_labels(category_names)
     names = [cat_to_name[c] for c in classes]
-    
+
     print('Probabilities (%)', [float(round(p * 100.0, 2)) for p in probs])
     print('Classes:', names)
-    
+
     print("Top most likely class:", names[0])
+
 
 def get_input_args():
     """
@@ -84,43 +98,51 @@ def get_input_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('input', help='Path to the image file to predict')
     parser.add_argument('checkpoint', help='Path to checkpoint file')
-    parser.add_argument('--category_names', type=str, default='cat_to_name.json', help='Path to classes map file')
-    parser.add_argument('--top_k', type=int, default=3, help='Number of highest probabilities')
-    parser.add_argument('--gpu', action='store_true', help='Utilize gpu to train')
+    parser.add_argument('--category_names', type=str,
+                        default='cat_to_name.json', help='Path to classes map file')
+    parser.add_argument('--top_k', type=int, default=3,
+                        help='Number of highest probabilities')
+    parser.add_argument('--gpu', action='store_true',
+                        help='Utilize gpu to train')
 
     return parser.parse_args()
+
 
 def predict(image_path, model, idx_to_class, topk=5, device='cpu'):
     '''
     Predict the class (or classes) of an image using a trained deep learning model.
     '''
-    print("Predicting the top {} classes with {} pre-trained model | device={}.".format(topk, model.__class__.__name__, device))
-    
-    # load an image file    
+    print("Predicting the top {} classes with {} pre-trained model | device={}.".format(topk,
+                                                                                        model.__class__.__name__, device))
+
+    # load an image file
     processed_image = utilities.process_image(image_path).squeeze()
-    
+
     # turn off drop-out
     model.eval()
     # change to cuda
     if device == 'gpu':
         model = model.cuda()
-    
+
     with torch.no_grad():
         if device == 'gpu':
-            output = model(torch.from_numpy(processed_image).float().cuda().unsqueeze_(0))
+            output = model(torch.from_numpy(
+                processed_image).float().cuda().unsqueeze_(0))
         else:
-            output = model(torch.from_numpy(processed_image).float().unsqueeze_(0))
-    
+            output = model(torch.from_numpy(
+                processed_image).float().unsqueeze_(0))
+
     # Calculate the class probabilities (softmax) for image
     ps = F.softmax(output, dim=1)
-    
+
     top = torch.topk(ps, topk)
-    
+
     probs = top[0][0].cpu().numpy()
     classes = [idx_to_class[i] for i in top[1][0].cpu().numpy()]
-    
+
     return probs, classes
+
 
 # Call to main function to run the program
 if __name__ == "__main__":
-    main() 
+    main()
